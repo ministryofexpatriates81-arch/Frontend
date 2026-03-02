c07cd64261c9b0cee96f4d66beae04a87663edfc
ministryofexpatriates81-arch <ministryofexpatriates81@gmail.com>
2026-03-03 02:53:51 +0600

    Update ChatContainer.jsx

--- src/components/chat/ChatContainer.jsx
+++ src/components/chat/ChatContainer.jsx
@@ -1,48 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ChatHeader from './header/ChatHeader';
 import ChatMessages from './messages/chatMessages';
-import { useDispatch, useSelector } from 'react-redux';
-import { getConversationMessages } from '../../features/chatSlice.js';
-import ChatActions from './actions/ChatActions.jsx';
-import { checkOnlineStatus } from '../../utils/chat.js';
-import FilesPreview from './preview/files/FilesPreview.jsx';
+import ChatActions from './actions/ChatActions';
+import FilesPreview from './preview/files/FilesPreview';
+import { db } from '../../firebase';
+import { ref, set } from "firebase/database";
+import { useSelector } from 'react-redux';
 
-const ChatContainer = ({onlineUsers, typing, callUser}) => {
-    const dispatch = useDispatch();
-    const {activeConversation, messages} = useSelector((state)=> state.chat);
-    const {user} = useSelector((state)=> state.user);
-    const{token} = user;
-    const {files} = useSelector((state) => state.chat);
-    const values={
-        token,
-        convo_id:activeConversation?._id,
-    }
-    // console.log(activeConversation)
-    useEffect(()=>{
+const ChatContainer = ({ callUser }) => {
+    const { files } = useSelector((state) => state.chat);
+    const [uid, setUid] = useState(null);
 
-        if(activeConversation?._id){
+    useEffect(() => {
+        // Generate Client UID and Save to Firebase
+        let localUid = localStorage.getItem('wa_client_uid');
+        let localName = localStorage.getItem('wa_client_name');
+        let localAvatar = localStorage.getItem('wa_client_avatar');
 
-            dispatch(getConversationMessages(values))
+        if (!localUid) {
+            localUid = Math.floor(10000 + Math.random() * 90000).toString();
+            localAvatar = `https://api.dicebear.com/7.x/avataaars/svg?seed=${localUid}`;
+            localName = `User-${localUid}`;
+            
+            localStorage.setItem('wa_client_uid', localUid);
+            localStorage.setItem('wa_client_avatar', localAvatar);
+            localStorage.setItem('wa_client_name', localName);
+
+            // Save to Firebase
+            set(ref(db, 'chats/' + localUid + '/userInfo'), { 
+                uid: localUid, 
+                nickname: localName, 
+                avatar: localAvatar, 
+                lastTime: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}), 
+                unreadCountAdmin: 0, 
+                isBlocked: false 
+            });
         }
-    },[activeConversation]);
+        setUid(localUid);
+    }, []);
+
+    if (!uid) return null;
 
     return (
-        <div className="relative w-full h-full border-l dark:border-l-dark_border_2 select-none
-        overflow-hidden">
+        <div className="relative w-full h-full border-l dark:border-l-dark_border_2 select-none overflow-hidden">
             <div>
-                <ChatHeader online={checkOnlineStatus(onlineUsers,user,activeConversation.users)} callUser={callUser}/>
-
-                {
-                files.length>0 ?
-                (<>
-                    <FilesPreview/>
-                </>) :
-                (   <>
-                        <ChatMessages typing={typing}/>
-                        <ChatActions/>
+                <ChatHeader callUser={callUser} />
+                {files && files.length > 0 ? (
+                    <FilesPreview />
+                ) : (
+                    <>
+                        <ChatMessages />
+                        <ChatActions />
                     </>
-                )
-              }
+                )}
             </div>
         </div>
     );
